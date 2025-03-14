4195b2119c24a2a5064d986cc54a169637b1dee8
Ibrahim <abdiwaliibrahimbulle@gmail.com>
2025-03-14 12:43:37 +0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+### 1.Faith Anyango -COM/B/01-00159/2023
+### 2.Ibrahim Bulle -COM/B/01-00096/2023
+### 3.
---
+++ script.js
@@ -0,0 +1,27 @@
+const error=document.querySelector(".error")
+const form=document.querySelector(".main")
+const password=document.querySelector(".password")
+const confirmPassword=document.querySelector(".cofpassword")
+form.addEventListener("submit",(e)=>{
+   e.preventDefault()
+   console.log(123);
+   console.log(password.value);
+   const newerror=[]
+   error.innerText = "";
+if(password.value!=confirmPassword.value){
+    newerror.push("Passwords should match")
+    }
+    
+if(password.value.length<8){
+    newerror.push("passwords should be atleast 8 characters")
+    
+}
+console.log(error);
+if(newerror.length>0){
+    error.innerText = newerror.join("\n");
+}else{
+    form.submit()
+}
+
+}
+)
